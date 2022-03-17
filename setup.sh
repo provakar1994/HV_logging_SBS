a6e0f430262d18093527ae396a8ff3c1e363c811
Provakar Datta <pdbforce@ifarm1901.jlab.org>
2022-03-17 16:35:56 -0400

    environment setup file added

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export HV_LOGGER_DIR=${PWD}
+export BB_HV=/adaqfs/home/aslow/EPICS/HV/bb_all
+export SBS_HV=/adaqfs/home/aslow/EPICS/HV/sbs_all
+export LHRS_HV=/adaqfs/home/aslow/EPICS/HV/lhrs_all
